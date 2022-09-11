2661b43bc8722228ee504cc47b93ee0cae331389
JCV00 <jorgeantoniocv@hotmail.com>
2022-09-11 02:41:51 -0500

    Joint

--- app/src/main/java/com/app/quantumrobotics/MainActivity.java
+++ app/src/main/java/com/app/quantumrobotics/MainActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.VideoView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class MainActivity extends AppCompatActivity {
     VideoView videoV;
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +41,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        initJointLay();
+    }
+
+    private void initJointLay(){
+        final LinearLayout joints_layout= findViewById(R.id.layoutJoints);
+        final BottomSheetBehavior<LinearLayout> bottomSheetBehavior= BottomSheetBehavior.from(joints_layout);
+        spinner=(Spinner)findViewById(R.id.spinner);
+        String [] joints={"1","2","3","4","5"};
+        ArrayAdapter <String> adapter= new ArrayAdapter<String>(this, R.layout.spinner_text,joints);
+        adapter.setDropDownViewResource(R.layout.dropdown);
+        spinner.setAdapter(adapter);
+
+        joints_layout.findViewById(R.id.layoutJoints).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(bottomSheetBehavior.getState()!=BottomSheetBehavior.STATE_EXPANDED){
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }else{
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+        });
     }
 }
